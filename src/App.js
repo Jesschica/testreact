c75315e6d84f403566a5d2cab1dcb6cc40540fdc
Jesschica <jessica.pean@neuf.fr>
2024-08-28 14:26:24 +0200

    add page router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
+
+export default function App(){
     return (
-      <div>
-        <h1>Titre</h1>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
       </div>
     )
-  }
-}
-
-export default App;
+  };
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact =() => {
+    return (
+        <div>
+            <h1>Page d'accueil</h1>
+        </div>
+    )
+}
+
+export default Contact;
