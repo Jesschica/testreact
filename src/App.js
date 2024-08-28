768138ff25d32373b776ec8b8477a84da3428762
Jesschica <jessica.pean@neuf.fr>
2024-08-28 14:49:52 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import Header from './Components/Header';
+
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route, Link } from "react-router-dom";
 
@@ -9,12 +12,16 @@ import Contact from './Pages/Contact';
 
 export default function App(){
     return (
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-        </Routes>
-      </div>
+      <body>
+        <Header />
+        
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </div>
+      </body>
     )
   };
---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function Header() {
+    return (
+        <hearder>
+            <h1>Header</h1>
+        </hearder>
+    )
+}
+
+export default Header;
